5bfb0ea6198d500affadc03e2d5c1dc8cb22722e
dtwc79 <dennis.thomas.wach@mnd.thm.de>
2022-07-05 21:09:46 +0200

    removed hover on "eingeloggt als" on navbar_search

--- includes/navbar_search.php
+++ includes/navbar_search.php
@@ -28,7 +28,7 @@ include 'logo.html';
                     <a class="nav_item" href="exercise_create.php">Übung erstellen</a>
                 </div>
             </div>
-            <?php echo '<a class="nav_item" style="width:30%;text-align:start;"> Eingeloggt als: ' . $_SESSION['user'] . '</a>'; ?>
+            <?php echo '<a class="logged_in"> Eingeloggt als: ' . $_SESSION['user'] . '</a>'; ?>
             <input id="search" size="9" onkeyup="search()" type="text" name="search" style="border:1px;border-radius:5px;margin-left:auto;margin-top:5px;
         margin-bottom:5px;margin-right:5px;text-align: center;" placeholder="Suche" autofocus>
             <a class="nav_item" href="profil.php">Profil</a>
