d58d4646bbe38c85749d110db970aa7d8d241f18
kejs84 <kai.erik.jesse@mnd.thm.de>
2022-07-10 20:36:08 +0200

    Changed JS show_form. Forms can be closed now.

--- js/show_form.js
+++ js/show_form.js
@@ -1,12 +1,21 @@
 function show_form_desc() {
   let x = document.getElementsByClassName("form_desc");
+  var str = "block";
+  if (x[0].style.display == "block"){
+    str = "none";
+  }
   for (i = 0; i < x.length; i++) {
-    x[i].style.display = "block";
+    x[i].style.display = str;
   }
 }
+
 function show_form_img() {
   let x = document.getElementsByClassName("form_img");
+  var str = "block";
+  if (x[0].style.display == "block"){
+    str = "none";
+  }
   for (i = 0; i < x.length; i++) {
-    x[i].style.display = "block";
+    x[i].style.display = str;
   }
 }
